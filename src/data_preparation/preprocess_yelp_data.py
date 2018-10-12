afa33d2afc35037cbda2421b127e42085db51923
Devershi Purohit <devership16@gmail.com>
2018-10-12 15:44:52 -0700

    added script for removing invalid states

--- src/data_preparation/preprocess_yelp_data.py
+++ src/data_preparation/preprocess_yelp_data.py
@@ -1,4 +1,5 @@
 import time
+import pandas as pd
 from INF553_YelpProject.src.utils.inputOutput_utils import load_yelp_data_json, csvWriter
 
 def load_lasVegas_data(data):
@@ -6,13 +7,31 @@ def load_lasVegas_data(data):
     data = data[data['city'].map(lambda x: x=="Las Vegas")]
     data = data[data['state'].map(lambda x: x=="NV")]
     return data 
+
+def clean_yelp_data(data):
+    us_states = {"AL" : "01", "AK" : "02", "AZ" : "04", "AR" : "05" ,"CA" : "06", "CO" : "08", "CT" : "09",
+                 "DE" : "10", "DC" : "11", "FL" : "12", "GA" : "13", "HI" : "15", "ID" : "16", "IL" : "17",
+                 "IN" : "18", "IA" : "19", "KS" : "20", "KY" : "21", "LA" : "22", "ME" : "23", "MD" : "24",
+                 "MA" : "25", "MI" : "26", "MN" : "27", "MS" : "28", "MO" : "29", "MT" : "30", "NE" : "31",
+                 "NV" : "32", "NH" : "33", "NJ" : "34", "NM" : "35", "NY" : "36", "NC" : "37", "ND" : "38",
+                 "OH" : "39", "OK" : "40", "OR" : "41", "PA" : "42", "RI" : "44", "SC" : "45", "SD" : "46",
+                 "TN" : "47", "TX" : "48", "UT" : "49", "VT" : "50", "VA" : "51", "WA" : "53", "WV" : "54",
+                 "WI" : "55", "WY" : "56"}
     
+    invalid_data = data[data['state'].map(lambda x: x not in us_states)]
+    data = data[data['state'].map(lambda x: x in us_states)]
+    
+    return data
+     
+
 def preprocess_yelp_data(path):
     
-    filePath = path + "yelp_academic_dataset_business.json"   
-    yelp_data = load_yelp_data_json(filePath)
-    lasVegasData = load_lasVegas_data(yelp_data)
-    csvWriter(path + "lasVegas.csv", lasVegasData)
+    file_path = path + "yelp_academic_dataset_business.json"   
+    yelp_data = load_yelp_data_json(file_path)
+#     lasVegasData = load_lasVegas_data(yelp_data)
+#     csvWriter(path + "lasVegas.csv", lasVegasData)
+    
+    yelp_data = clean_yelp_data(yelp_data)
     
 if __name__=='__main__':   
     
