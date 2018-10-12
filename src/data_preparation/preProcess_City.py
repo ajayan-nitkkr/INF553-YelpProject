91d2fcb691f9815a05a1bd409c7f54c833fa0813
Devershi Purohit <devership16@gmail.com>
2018-10-12 15:01:34 -0700

    Updated preprocessing script and added utils

--- src/data_preparation/preProcess_City.py
+++ src/data_preparation/preProcess_City.py
@@ -1,96 +1,24 @@
-import json
 import time
-import pandas as pd
-from collections import defaultdict
+from INF553_YelpProject.src.utils.inputOutput_utils import load_yelp_data_json, csvWriter
 
 ############################################################### File Operations
 
-def jsonReader(path):
-    file = []
-    if (path):
-        jsonFin = open(path, "r", encoding = "utf-8")
-        jsonData = {}
-        index = 0
-        statesData = defaultdict(list)
-        statesCount = defaultdict(int)
-        inValidData = defaultdict(list)
-        cityCount = defaultdict(int)
-        lasVegasData = []
-        for line in jsonFin:
-            record = json.loads(line)
-            
-            cityCount[(record['city'],record['state'])]+=1
-            
-            #Removing ON from states
-            
-            if len(record['state'])<2 or len(record['state'])>2 or record['state'].isdigit():
-                
-                if record['state']=='C':
-                    record['state']="CA"
-                    statesData[record['state']].append(record)
-
-                else:
-                    inValidData[record['business_id']] = [record['state'], record['city'],record['postal_code']] 
-                
-            elif len(record['state']) == 2:
-                statesData[record['state']].append(record)
-                statesCount[record['state']]+=1
-                
-            if record['state']=="NV" and record['city']=="Las Vegas":
-                lasVegasData.append(record)
-    
-#     for key in inValidData.keys():
-#         print(key, inValidData[key])
-        
-    #print(len(statesData), len(inValidData))
-
-#     for key in sorted(statesCount, key = statesCount.get, reverse  = True):
-#         print(key, statesCount[key])
-#     for key in sorted(cityCount, key = cityCount.get, reverse  = True):
-#         print(key, cityCount[key])
-
-
-#     for key in sorted(statesData):
-#         print(key+" : "+ ", ".join(statesData[key]))
-    
-    return lasVegasData 
-
-
-def load_yelp_challenge_data(file):
-    
-    with open(file,'rb') as jsonFin:
-        data = pd.DataFrame(json.loads(line) for line in jsonFin)
+def load_lasVegas_data(data):
     
     data = data[data['city'].map(lambda x: x=="Las Vegas")]
     data = data[data['state'].map(lambda x: x=="NV")]
     
-    print("Yelp Data: "+str(len(data))) 
     return data 
 
-def csvWriter(path, data):
-    data.to_csv(path, encoding = "utf-8")
-
-def jsonWriter(path,lasVegasData):
-    jsonFout = open(path+file, "w",encoding ="utf-8")
-    for element in lasVegasData:
-        jsonFout.write(str(element)+"\n")
-    jsonFout.close()
     
-    return
-    
-def preprocessJson(path):
+def preprocess_yelp_data(path):
     
     filePath = path + "yelp_academic_dataset_business.json"
     #filePath = path + "temp_business.json"
     
-    #lasVegasData = jsonReader(filePath)
-    lasVegasData = load_yelp_challenge_data(filePath)
-    csvWriter(path+"lasVegas.csv", lasVegasData)
-    
-    data = pd.read_csv(path+"lasVegas.csv")
-    
-    #jsonWriter(path+"lasVegas.json", lasVegasData)
-    
+    yelp_data = load_yelp_data_json(filePath)
+    lasVegasData = load_lasVegas_data(yelp_data)
+    csvWriter(path + "lasVegas.csv", lasVegasData)
     
      
 
@@ -99,7 +27,7 @@ if __name__=='__main__':
     start=time.time()
     
     path = "../../../data/yelp_dataset/"
-    preprocessJson(path) 
+    preprocess_yelp_data(path) 
     
     print ("\nRun time: "+ str(time.time()-start)+" seconds" )  
     
---
+++ src/utils/inputOutput_utils.py
@@ -0,0 +1,13 @@
+import json
+import pandas as pd
+
+def load_yelp_data_json(file):
+    
+    with open(file,'rb') as jsonFin:
+        data = pd.DataFrame(json.loads(line) for line in jsonFin)    
+    
+    return data 
+
+def csvWriter(path, data):
+    data.to_csv(path, encoding = "utf-8")
+
