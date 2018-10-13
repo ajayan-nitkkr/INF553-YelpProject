51bd5fc4f366634623941b257a90724e61ce8ffe
Ajay Anand <anandajay1834@gmail.com>
2018-10-13 10:57:38 -0700

    modify api to extract business url and use it to srcap basic restaurant data

--- src/data_preparation/yelp/main.py
+++ src/data_preparation/yelp/main.py
@@ -1,9 +1,11 @@
 from src.data_preparation.yelp.yelp_api_adapter import search_yelp_restaurant
+from src.data_preparation.yelp.yelp_scraper import scrap_yelp_restaurant_data
 
 if __name__ == '__main__':
     business_id = 'hTzcHtk4-0QJnFUbkKpd5Q'
     name = 'Citi Trends'
     latitude = '36.1783477'
     longitude = '-115.1769162'
-    data = search_yelp_restaurant(business_id)
-    print(data)
+    business_data = search_yelp_restaurant(business_id)
+    scraped_data = scrap_yelp_restaurant_data(business_data['url'])
+    print(scraped_data)
--- src/data_preparation/yelp/yelp_scraper.py
+++ src/data_preparation/yelp/yelp_scraper.py
@@ -1,12 +1,15 @@
 import urllib
+import requests
 from bs4 import BeautifulSoup
 
 """
 Scrap restaurant data from yelp website based on restaurant url
 """
 def scrap_yelp_restaurant_data(restaurant_url):
-    yelp_path = urllib.urlopen(restaurant_url).read()
-    soup = BeautifulSoup(yelp_path, "html.parser")
-
+    # yelp_path = urllib.urlopen(restaurant_url).read()
+    yelp_path = requests.get(restaurant_url, verify=False)
+    soup = BeautifulSoup(yelp_path.text)
+    print(soup.title)
     # my_dict = {}
     basic_data = soup.find('script', type='application/ld+json')
+    print(basic_data)
