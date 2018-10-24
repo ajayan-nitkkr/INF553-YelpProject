07ec7eb38d6dbd4de7d13d0b604070816a4fbb6b
Ajay Anand <anandajay1834@gmail.com>
2018-10-24 16:36:58 -0700

    creating feature names for input features and final schema

---
+++ src/data_preparation/input_data_schema.py
@@ -0,0 +1,67 @@
+"""
+Author: Ajay Anand
+"""
+class YelpBusinessDataSchema:
+    COL_BUSINESS_ID = "BusinessId"
+    COL_RESTAURANT_NAME = "RestaurantName"
+    COL_NEIGHBORHOOD = "Neighborhood"
+    COL_ADDRESS = "Address"
+    COL_CITY = "City"
+    COL_STATE = "State"
+    COL_ZIP = "Zip"
+    COL_STARS = "Stars"
+    COL_REVIEW_COUNT = "ReviewCount"
+    COL_ACCEPTS_INSURANCE = "AcceptsInsurance"
+    COL_AGES_ALLOWED = "AgesAllowed"
+    COL_ALCOHOL = "Alcohol"
+    COL_AMBIENCE = "Ambience" # have to check the type
+    COL_BYOB = "BYOB"
+    COL_BYOB_Corkage = "BYOBCorkage"
+    COL_BEST_NIGHTS = "BestNights" # have to check the type
+    COL_BIKE_PARKING = "BikeParking"
+    COL_BUSINESS_ACCEPTS_BITCOIN = "BusinessAcceptsBitcoin"
+    COL_BUSINESS_ACCEPTS_CREDITCARDS = "BusinessAcceptsCreditCards"
+    COL_BUSINESS_PARKING = "BusinessParking" # have to check the type
+    COL_BYAPPOINTMENTONLY = "ByAppointmentOnly"
+    COL_CATERS = "Caters"
+    COL_COAT_CHECK = "CoatCheck"
+    COL_CORKAGE = "Corkage"
+    COL_DIETARY_RESTRICTIONS = "DietaryRestrictions" # have to check the type
+    COL_DOGS_ALLOWED = "DogsAllowed"
+    COL_DRIVE_THRU = "DriveThru"
+    COL_GOOD_FOR_DANCING = "GoodForDancing"
+    COL_GOOD_FOR_KIDS = "GoodForKids"
+    COL_GOOD_FOR_MEAL = "GoodForMeal" # have to check the type
+    COL_HAIR_SPECIALIZES_IN = "HairSpecializesIn" # have to check the type
+    COL_HAPPY_HOUR = "HappyHour"
+    COL_HAS_TV = "HasTV"
+    COL_MUSIC = "Music" # have to check the type
+    COL_NOISE_LEVEL = "NoiseLevel"
+    COL_OPEN_24_HOURS = "Open24Hours"
+    COL_OUTDOOR_SEATING = "OutdoorSeating"
+    COL_RESTAURANTS_ATTIRE = "RestaurantsAttire"
+    COL_RESTAURANTS_COUNTER_SERVICE = "RestaurantsCounterService"
+    COL_RESTAURANTS_DELIVERY = "RestaurantsDelivery"
+    COL_RESTAURANTS_GOOD_FOR_GROUPS = "RestaurantsGoodForGroups"
+    COL_RESTAURANTS_PRICE_RANGE2 = "RestaurantsPriceRange2"
+    COL_RESTAURANTS_RESERVATIONS = "RestaurantsReservations"
+    COL_RESTAURANTS_TABLE_SERVICE = "RestaurantsTableService"
+    COL_RESTAURANTS_TAKEOUT = "RestaurantsTakeOut"
+    COL_SMOKING = "Smoking"
+    CoL_WHEELCHAIR_ACCESSIBLE = "WheelchairAccessible"
+    COL_WIFI = "WiFi"
+
+
+class LasVegasGovtDataSchema:
+    COL_RESTAURANT_NAME = "RestaurantName"
+    COL_LOCATION_NAME = "LocationName"
+    COL_CATEGORY_NAME = "CategoryName"
+    COL_ADDRESS = "Address"
+    COL_CITY = "City"
+    COL_STATE = "State"
+    COL_ZIP = "Zip"
+    COL_CURRENT_GRADE = "CurrentGrade"
+    COL_INSPECTION_GRADE = "InspectionGrade"
+    COL_VIOLATIONS = "Violations"
+    COL_CURRENT_DEMERITS = "CurrentDemerits"
+    COL_INSPECTION_DEMERITS = "InspectionDemerits"
--- src/data_schema/feature_names.py
+++ src/data_schema/feature_names.py
@@ -2,16 +2,62 @@
 Author: Ajay Anand
 """
 
-COL_BUSINESS_ID = 'business_id'
-COL_NAME = 'name'
-COL_ADDRESS = 'address'
-COL_CITY = 'city'
-COL_STATE = 'state'
-COL_ZIP = 'zip_code'
-COL_IS_OPEN = 'is_open'
-COL_REVIEW_COUNT = 'review_count'
-COL_RATING = 'rating'
-COL_HEALTH_SCORE = 'health_score'
-COL_DESCRIPTION = 'description'
+class FeatureNames:
+    COL_BUSINESS_ID = 'business_id'
+    COL_NAME = 'name'
+    COL_NEIGHBORHOOD = "neighborhood"
+    COL_ADDRESS = 'address'
+    COL_CITY = 'city'
+    COL_STATE = 'state'
+    COL_ZIP = 'zip_code'
+    COL_REVIEW_COUNT = 'review_count'
+    COL_RATING = 'rating'
+    COL_ACCEPTS_INSURANCE = "accepts_insurance"
+    COL_AGES_ALLOWED = "ages_allowed"
+    COL_ALCOHOL = "alcohol"
+    COL_AMBIENCE = "ambience" # have to check the type
+    COL_BYOB = "byob"
+    COL_BYOB_Corkage = "byob_corkage"
+    COL_BEST_NIGHTS = "best_nights" # have to check the type
+    COL_BIKE_PARKING = "bike_parking"
+    COL_BUSINESS_ACCEPTS_BITCOIN = "business_accepts_bitcoin"
+    COL_BUSINESS_ACCEPTS_CREDITCARDS = "business_accepts_creditcards"
+    COL_BUSINESS_PARKING = "business_parking" # have to check the type
+    COL_BYAPPOINTMENTONLY = "byappointmentonly"
+    COL_CATERS = "caters"
+    COL_COAT_CHECK = "coat_check"
+    COL_CORKAGE = "corkage"
+    COL_DIETARY_RESTRICTIONS = "dietary_restrictions" # have to check the type
+    COL_DOGS_ALLOWED = "dogs_allowed"
+    COL_DRIVE_THRU = "drive_thru"
+    COL_GOOD_FOR_DANCING = "good_for_dancing"
+    COL_GOOD_FOR_KIDS = "good_for_kids"
+    COL_GOOD_FOR_MEAL = "good_for_meal" # have to check the type
+    COL_HAIR_SPECIALIZES_IN = "hair_specializes_in" # have to check the type
+    COL_HAPPY_HOUR = "happy_hour"
+    COL_HAS_TV = "has_tv"
+    COL_MUSIC = "music" # have to check the type
+    COL_NOISE_LEVEL = "noise_level"
+    COL_OPEN_24_HOURS = "open_24_hours"
+    COL_OUTDOOR_SEATING = "outdoor_seating"
+    COL_RESTAURANTS_ATTIRE = "restaurants_attire"
+    COL_RESTAURANTS_COUNTER_SERVICE = "restaurants_counter_service"
+    COL_RESTAURANTS_DELIVERY = "restaurants_delivery"
+    COL_RESTAURANTS_GOOD_FOR_GROUPS = "restaurants_good_for_groups"
+    COL_RESTAURANTS_PRICE_RANGE2 = "restaurants_price_range2"
+    COL_RESTAURANTS_RESERVATIONS = "restaurants_reservations"
+    COL_RESTAURANTS_TABLE_SERVICE = "restaurants_table_service"
+    COL_RESTAURANTS_TAKEOUT = "restaurants_takeout"
+    COL_SMOKING = "smoking"
+    CoL_WHEELCHAIR_ACCESSIBLE = "wheelchair_accessible"
+    COL_WIFI = "wifi"
 
-COl_PRICE = 'price'
+    COL_LOCATION_NAME = "location_name" # what is meant by this feature
+    COL_CATEGORY_NAME = "category_name"
+    COL_CURRENT_GRADE = "current_grade"
+    COL_INSPECTION_GRADE = "inspection_grade"
+    COL_VIOLATIONS = "violations"
+    COL_CURRENT_DEMERITS = "current_demerits"
+    COL_INSPECTION_DEMERITS = "inspection_demerits"
+
+    COL_IS_OPEN = 'is_open' # have to check if needed
