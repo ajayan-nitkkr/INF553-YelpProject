eb31b6daa7ea9de19375c0462581b8934291d3af
Devershi Purohit <devership16@gmail.com>
2018-10-25 15:08:44 -0700

    added detailed analysis for missing data(full, partial)

--- src/data_analysis/missing_data_yelp_business.py
+++ src/data_analysis/missing_data_yelp_business.py
@@ -21,11 +21,61 @@ def find_business_with_missing_data(data):
        
     return missing_data
 
+
+def missing_data_details(data):
+    
+    data_schema = ['name', "neighborhood", 'address', 'city', 'state', 'postal_code', 'review_count',
+                   'latitude', "longitude", "stars", "is_open", "attributes", "categories", "hours" ]
+    
+    full_data_bIds = []
+    partial_data_bIds = defaultdict(list)
+    
+    for index,row in data.iterrows():
+        full = True
+        b_id = row['business_id']
+        for col in data_schema:
+            if row[col]==True:
+                partial_data_bIds[b_id].append(col)
+                full = False
+        if  full:
+            full_data_bIds.append(b_id)
+
+    return full_data_bIds, partial_data_bIds
+
+def print_full_data_details(data, path):
+    fout =  open(path+"business_with_full_data.txt", "w+", encoding = "utf-8")
+    
+    for b_id in data:
+        fout.write(str(b_id) + "\n")
+    
+    fout.close()
+    return
+
+def print_partial_data_details(data, path):
+    fout =  open(path+"business_with_partial_data.csv", "w+", encoding = "utf-8")
+    
+    for key in data.keys():
+        fout.write(str(key))
+        for col in data[key]: 
+            fout.write("," + str(col))
+        
+        fout.write("\n")
+        
+    fout.close()
+    return
+    
 def find_missing_data(path):
     yelp_data = csvReader(path+"preprocessed_lasVegas.csv")
     
     business_with_missing_data = find_business_with_missing_data(yelp_data)
-    csvWriter(path+"missing_data_yelp_business.csv", business_with_missing_data)
+#     csvWriter(path+"missing_data_yelp_business.csv", business_with_missing_data)
+    
+    full_data_bIds, partial_data_bIds = missing_data_details(business_with_missing_data)
+    
+    print(len(full_data_bIds), len(partial_data_bIds))
+
+    print_full_data_details(full_data_bIds, path)
+    print_partial_data_details(partial_data_bIds, path)
     
     return 
 
