7ffdee2f8800f4182875dab86cd3b558ba5f1440
Ajay Anand <anandajay1834@gmail.com>
2018-10-25 11:19:15 -0700

    rename yelp challenge business features

--- src/data_preparation/feature_name_prep.py
+++ src/data_preparation/feature_name_prep.py
@@ -4,7 +4,7 @@ Author: Ajay Anand
 
 import pandas as pd
 
-from src.data_preparation.input_data_schema import LasVegasGovtDataSchema
+from src.data_preparation.input_data_schema import LasVegasGovtDataSchema, YelpBusinessDataSchema
 from src.data_schema.feature_names import FeatureNames
 from src.utils.inputOutput_utils import csvWriter
 
@@ -27,9 +27,71 @@ def rename_govt_data_features(df):
     return df
 
 
+def rename_yelp_challenge_features(df):
+    yelp_obj = YelpBusinessDataSchema()
+    schema_obj = FeatureNames()
+    df.rename(columns={yelp_obj.COL_BUSINESS_ID: schema_obj.COL_BUSINESS_ID}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANT_NAME: schema_obj.COL_NAME}, inplace=True)
+    df.rename(columns={yelp_obj.COL_NEIGHBORHOOD: schema_obj.COL_NEIGHBORHOOD}, inplace=True)
+    df.rename(columns={yelp_obj.COL_ADDRESS: schema_obj.COL_ADDRESS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_CITY: schema_obj.COL_CITY}, inplace=True)
+    df.rename(columns={yelp_obj.COL_STATE: schema_obj.COL_STATE}, inplace=True)
+    df.rename(columns={yelp_obj.COL_ZIP: schema_obj.COL_ZIP}, inplace=True)
+    df.rename(columns={yelp_obj.COL_STARS: schema_obj.COL_RATING}, inplace=True)
+    df.rename(columns={yelp_obj.COL_REVIEW_COUNT: schema_obj.COL_REVIEW_COUNT}, inplace=True)
+    df.rename(columns={yelp_obj.COL_ACCEPTS_INSURANCE: schema_obj.COL_ACCEPTS_INSURANCE}, inplace=True)
+    df.rename(columns={yelp_obj.COL_AGES_ALLOWED: schema_obj.COL_AGES_ALLOWED}, inplace=True)
+    df.rename(columns={yelp_obj.COL_ALCOHOL: schema_obj.COL_ALCOHOL}, inplace=True)
+    df.rename(columns={yelp_obj.COL_AMBIENCE: schema_obj.COL_AMBIENCE}, inplace=True)
+    df.rename(columns={yelp_obj.COL_BYOB: schema_obj.COL_BYOB}, inplace=True)
+    df.rename(columns={yelp_obj.COL_BYOB_Corkage: schema_obj.COL_BYOB_Corkage}, inplace=True)
+    df.rename(columns={yelp_obj.COL_BEST_NIGHTS: schema_obj.COL_BEST_NIGHTS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_BIKE_PARKING: schema_obj.COL_BIKE_PARKING}, inplace=True)
+    df.rename(columns={yelp_obj.COL_BUSINESS_ACCEPTS_BITCOIN: schema_obj.COL_BUSINESS_ACCEPTS_BITCOIN}, inplace=True)
+    df.rename(columns={yelp_obj.COL_BUSINESS_ACCEPTS_CREDITCARDS: schema_obj.COL_BUSINESS_ACCEPTS_CREDITCARDS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_BUSINESS_PARKING: schema_obj.COL_BUSINESS_PARKING}, inplace=True)
+    df.rename(columns={yelp_obj.COL_BYAPPOINTMENTONLY: schema_obj.COL_BYAPPOINTMENTONLY}, inplace=True)
+    df.rename(columns={yelp_obj.COL_CATERS: schema_obj.COL_CATERS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_COAT_CHECK: schema_obj.COL_COAT_CHECK}, inplace=True)
+    df.rename(columns={yelp_obj.COL_CORKAGE: schema_obj.COL_CORKAGE}, inplace=True)
+    df.rename(columns={yelp_obj.COL_DIETARY_RESTRICTIONS: schema_obj.COL_DIETARY_RESTRICTIONS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_DOGS_ALLOWED: schema_obj.COL_DOGS_ALLOWED}, inplace=True)
+    df.rename(columns={yelp_obj.COL_DRIVE_THRU: schema_obj.COL_DRIVE_THRU}, inplace=True)
+    df.rename(columns={yelp_obj.COL_GOOD_FOR_DANCING: schema_obj.COL_GOOD_FOR_DANCING}, inplace=True)
+    df.rename(columns={yelp_obj.COL_GOOD_FOR_KIDS: schema_obj.COL_GOOD_FOR_KIDS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_GOOD_FOR_MEAL: schema_obj.COL_GOOD_FOR_MEAL}, inplace=True)
+    df.rename(columns={yelp_obj.COL_HAIR_SPECIALIZES_IN: schema_obj.COL_HAIR_SPECIALIZES_IN}, inplace=True)
+    df.rename(columns={yelp_obj.COL_HAPPY_HOUR: schema_obj.COL_HAPPY_HOUR}, inplace=True)
+    df.rename(columns={yelp_obj.COL_HAS_TV: schema_obj.COL_HAS_TV}, inplace=True)
+    df.rename(columns={yelp_obj.COL_MUSIC: schema_obj.COL_MUSIC}, inplace=True)
+    df.rename(columns={yelp_obj.COL_NOISE_LEVEL: schema_obj.COL_NOISE_LEVEL}, inplace=True)
+    df.rename(columns={yelp_obj.COL_OPEN_24_HOURS: schema_obj.COL_OPEN_24_HOURS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_OUTDOOR_SEATING: schema_obj.COL_OUTDOOR_SEATING}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANTS_ATTIRE: schema_obj.COL_RESTAURANTS_ATTIRE}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANTS_COUNTER_SERVICE: schema_obj.COL_RESTAURANTS_COUNTER_SERVICE}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANTS_DELIVERY: schema_obj.COL_RESTAURANTS_DELIVERY}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANTS_GOOD_FOR_GROUPS: schema_obj.COL_RESTAURANTS_GOOD_FOR_GROUPS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANTS_PRICE_RANGE2: schema_obj.COL_RESTAURANTS_PRICE_RANGE2}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANTS_RESERVATIONS: schema_obj.COL_RESTAURANTS_RESERVATIONS}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANTS_TABLE_SERVICE: schema_obj.COL_RESTAURANTS_TABLE_SERVICE}, inplace=True)
+    df.rename(columns={yelp_obj.COL_RESTAURANTS_TAKEOUT: schema_obj.COL_RESTAURANTS_TAKEOUT}, inplace=True)
+    df.rename(columns={yelp_obj.COL_SMOKING: schema_obj.COL_SMOKING}, inplace=True)
+    df.rename(columns={yelp_obj.CoL_WHEELCHAIR_ACCESSIBLE: schema_obj.CoL_WHEELCHAIR_ACCESSIBLE}, inplace=True)
+    df.rename(columns={yelp_obj.COL_WIFI: schema_obj.COL_WIFI}, inplace=True)
+    return df
+
+
 if __name__ == '__main__':
     govt_input_file = '../../resources/dataset/Restaurant_Inspections_CurrentGrade.csv'
     govt_output_file = '../../resources/dataset/Restaurant_Inspections_CurrentGrade_new.csv'
-    govt_df = pd.read_csv(govt_input_file)
-    govt_df = rename_govt_data_features(govt_df)
-    csvWriter(govt_output_file, govt_df)
+
+    # govt_df = pd.read_csv(govt_input_file)
+    # govt_df = rename_govt_data_features(govt_df)
+    # csvWriter(govt_output_file, govt_df)
+
+    yelp_challenge_input_file = '../../resources/dataset/Business_Preprocessed.csv'
+    yelp_challenge_output_file = '../../resources/dataset/Business_Preprocessed_new.csv'
+
+    yelp_df = pd.read_csv(yelp_challenge_input_file)
+    yelp_df = rename_yelp_challenge_features(yelp_df)
+    csvWriter(yelp_challenge_output_file, yelp_df)
