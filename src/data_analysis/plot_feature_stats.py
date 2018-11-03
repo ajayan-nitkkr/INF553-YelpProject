c8a40095a22e5fd335255bceb93ed710063dbfc7
Ajay Anand <anandajay1834@gmail.com>
2018-11-03 12:39:11 -0700

    enhance visualization script to plot features correctly with stat results

--- src/data_analysis/plot_feature_stats.py
+++ src/data_analysis/plot_feature_stats.py
@@ -8,22 +8,29 @@ from src.data_preparation.input_data_schema import LasVegasGovtDataSchema
 def plot_feature_stat(df, feature_xaxis, feature_yaxis, output_file):
     ##### construct list of mean, standard deviation, max values,###
     # min values, used for graph datapoints #####
-    mean_df = df.groupby(feature_xaxis, as_index=False)[feature_yaxis].mean()
+    groups_df = df.groupby([feature_xaxis])
+    # mean_df = df.groupby(feature_xaxis, as_index=False)[feature_yaxis].mean()
+    mean_df = groups_df.mean()
     mean_list = mean_df[feature_yaxis]
-    feature_list = mean_df[feature_xaxis]
+    feature_list = df.groupby([feature_xaxis])[feature_xaxis]
 
-    sd_df = df.groupby(feature_xaxis, as_index=False)[feature_yaxis].std()
+    # sd_df = df.groupby(feature_xaxis, as_index=False)[feature_yaxis].std()
+    sd_df = groups_df.std()
+    # df.groupby([feature_xaxis]).std()
     sd_list = sd_df[feature_yaxis]
 
-    min_df = df.groupby(feature_xaxis, as_index=False)[feature_yaxis].min()
+    # min_df = df.groupby(feature_xaxis, as_index=False)[feature_yaxis].min()
+    min_df = groups_df.min()
     min_list = min_df[feature_yaxis]
 
-    max_df = df.groupby(feature_xaxis, as_index=False)[feature_yaxis].max()
+    # max_df = df.groupby(feature_xaxis, as_index=False)[feature_yaxis].max()
+    max_df = groups_df.max()
     max_list = max_df[feature_yaxis]
 
     #### plot the mean, standard deviation, max value, min value in graph #####
-    plt.errorbar(np.arange(len(feature_list)), mean_list, sd_list, fmt='ok', lw=3, color='blue' )
-    plt.errorbar(np.arange(len(feature_list)), mean_list, [mean_list - min_list, max_list - mean_list],
+    plt.errorbar(np.arange(len(feature_list)), mean_list.values, sd_list.values, fmt='ok', ecolor='blue', lw=3)
+    plt.errorbar(np.arange(len(feature_list)), mean_list.values,
+                 [mean_list.values - min_list.values, max_list.values - mean_list.values],
                  fmt='.k', ecolor='gray', lw=1)
 
     #### Round off the score to two decimal places to be displayed in the graph #####
@@ -35,23 +42,25 @@ def plot_feature_stat(df, feature_xaxis, feature_yaxis, output_file):
         max_list[i] = round(max_list[i],2)
 
     #### annonate the values of datapoint labels in the graph ######
-    for xy in zip(feature_list, mean_list):
+    for xy in zip(np.arange(len(feature_list)), mean_list.values):
         plt.annotate('(%s, %s)' % xy, xy=xy, textcoords='data')
-    for xy in zip(feature_list, min_list):
+    for xy in zip(np.arange(len(feature_list)), min_list.values):
         plt.annotate('(%s, %s)' % xy, xy=xy, textcoords='data')
-    for xy in zip(feature_list, max_list):
+    for xy in zip(np.arange(len(feature_list)), max_list.values):
         plt.annotate('(%s, %s)' % xy, xy=xy, textcoords='data')
 
-    #### display the label on x and y axis #####
+    #### display/save the label on x and y axis #####
     plt.xlabel(feature_xaxis)
     plt.ylabel(feature_yaxis)
-    plt.show()
+    # plt.show()
+    plt.savefig(output_file)
 
 
 if __name__ == '__main__':
-    file = '../../resources/dataset/Restaurant_Inspections_CurrentGrade.csv'
-    output_file = '../../resources/images/price_stat.csv'
+    file = '../../resources/dataset/final_lasvegas_dataset.csv'
+    output_file = '../../resources/images/graphs/price.png'
+
     df = pd.read_csv(file)
     schema_obj = FeatureNames()
-    df = df[[schema_obj.COL_RESTAURANTS_PRICE_RANGE2, schema_obj.COL_CURRENT_GRADE]]
-    plot_feature_stat(df, schema_obj.COL_RESTAURANTS_PRICE_RANGE2, schema_obj.COL_CURRENT_GRADE, output_file)
+    df = df[[schema_obj.COL_RESTAURANTS_PRICE_RANGE2, schema_obj.COL_INSPECTION_SCORE]]
+    plot_feature_stat(df, schema_obj.COL_RESTAURANTS_PRICE_RANGE2, schema_obj.COL_INSPECTION_SCORE, output_file)
