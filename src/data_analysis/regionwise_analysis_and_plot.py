1ba26b69cbf4d36b46b47d3b923f13287e8ed2be
Devershi Purohit <devership16@gmail.com>
2018-11-02 18:32:50 -0700

    Task updates

--- src/data_analysis/regionwise_analysis_and_plot.py
+++ src/data_analysis/regionwise_analysis_and_plot.py
@@ -4,7 +4,14 @@ from INF553_YelpProject.src.utils.inputOutput_utils import csvReader, csvWriter
 
 
 def region_wise_mean_score(path):
-    pass
+    las_vegas_data = csvReader(path+"final_lasvegas_dataset.csv")
+    postal_code_avg = las_vegas_data.groupby('zip_code', as_index = False)['current_score'].mean()
+    
+    
+    csvWriter(path + "postal_code_wise_analysis.csv", postal_code_avg)
+    
+    return
+
 
 if __name__=='__main__':   
     
