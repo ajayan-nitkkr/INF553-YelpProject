9db5b08b46b07b98ed5c5e2a3b0328ca85ee8eaa
Ajay Anand <anandajay1834@gmail.com>
2018-11-06 13:19:37 -0800

    base code for training and prediction using svm linear kernel

--- src/machine_learning/svm_linearkernel_model.py
+++ src/machine_learning/svm_linearkernel_model.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn import svm
 from src.data_schema.feature_names import FeatureNames
+from src.machine_learning.evaluation_metrics import EvaluationMetric
 
 
 def construct_dataset():
@@ -11,7 +14,7 @@ def construct_dataset():
     df = df[[schema_obj.COL_RATING,
              schema_obj.COL_RESTAURANTS_PRICE_RANGE2,
              schema_obj.COL_INSPECTION_GRADE]]
-    # df[schema_obj.COL_INSPECTION_GRADE] = df.apply(categorize_score_attribute_with_boolean, axis=1)
+    df[schema_obj.COL_RESTAURANTS_PRICE_RANGE2] = df.apply(categorize_price_as_integer, axis=1)
 
     print("Dataset Shape:: ", df.shape)
     return df
@@ -50,6 +53,31 @@ def categorize_score_attribute_with_boolean(row):
     return modified_score
 
 
+def categorize_price_as_integer(row):
+    schema_obj = FeatureNames()
+    attribute_name = schema_obj.COL_RESTAURANTS_PRICE_RANGE2
+    modified_price = None
+
+    if row[attribute_name] == "Null": modified_price = -1
+    else: modified_price = int(row[attribute_name])
+    return modified_price
+
+
+def training_svm(X_train, y_train):
+    print("Training the data")
+    y_train_new = np.array([item for sublist in np.array(y_train).tolist() for item in sublist])
+    svm_clf = svm.SVC(kernel='linear')
+    svm_clf.fit(X_train, y_train_new)
+    print('Model:', svm_clf)
+    print("Training completed...")
+    return svm_clf
+
+
+def predict_testdata(svm_clf, X_test):
+    y_pred = svm_clf.predict(X_test)
+    return y_pred
+
+
 if __name__ == '__main__':
 
     ########### CONSTRUCT DATA SET ############
@@ -58,5 +86,19 @@ if __name__ == '__main__':
 
     ############# DATA SLICING ################
     X_train, X_test, y_train, y_test = divide_dataset(df)
-    print(X_train,X_test,y_train,y_test)
+    # print(X_train,X_test,y_train,y_test)
+    ###########################################
+
+    ################ TRAINING #################
+    svm_clf = training_svm(X_train, y_train)
+    ###########################################
+
+    ################ PREDICTION ###############
+    y_pred = predict_testdata(svm_clf, X_test)
+    ###########################################
+
+    ################ ACCURACY #################
+    evaluation_metric = EvaluationMetric()
+    result = evaluation_metric.get_evaluation_metrics(y_test, y_pred)
+    print(result)
     ###########################################
