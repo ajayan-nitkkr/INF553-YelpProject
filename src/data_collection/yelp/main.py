86714dad207f35270f8b825e79895f68b3af0d3d
Ajay Anand <anandajay1834@gmail.com>
2018-11-08 01:06:27 -0800

    final scraped data for restaurant in Las vegas

--- src/data_collection/yelp/main.py
+++ src/data_collection/yelp/main.py
@@ -4,20 +4,22 @@ Author: Ajay Anand
 
 import json
 import pandas as pd
+import time
 from src.data_collection.yelp.yelp_api_adapter import search_yelp_restaurant
 from src.data_collection.yelp.yelp_scraper import scrap_yelp_restaurant_data
-from src.data_preparation.input_data_schema import YelpBusinessDataSchema
+from src.data_schema.feature_names import FeatureNames
+# from src.data_preparation.input_data_schema import YelpBusinessDataSchema
 # from src.data_collection.yelp.yelp_feature_names import *
 
 
 def traverse_business_data(yelp_df):
-    yelp_obj = YelpBusinessDataSchema()
+    schema_obj = FeatureNames()
     restaurant_list = []
     for index, row in yelp_df.iterrows():
         print("index:",index)
-        if index >= 50:
-            break
-        business_id = row[yelp_obj.COL_BUSINESS_ID]
+        # if index >= 10:
+        #     break
+        business_id = row[schema_obj.COL_BUSINESS_ID]
         if business_id:
             business_data = search_yelp_restaurant(business_id)
             if not(business_data and type(business_data) is dict):
@@ -36,6 +38,7 @@ def traverse_business_data(yelp_df):
 
 
 if __name__ == '__main__':
+    program_start_time = round(time.time() * 1000)
     # business_id = 'hTzcHtk4-0QJnFUbkKpd5Q'
     # name = 'Citi Trends'
     # latitude = '36.1783477'
@@ -43,12 +46,16 @@ if __name__ == '__main__':
     # business_data = search_yelp_restaurant(business_id)
     # scraped_data = scrap_yelp_restaurant_data(business_data['url'])
     # print(scraped_data)
-    yelp_business_file = '../../../resources/dataset/LasVegas_Restaurants_Rimsha_Preprocessed.csv'
+    # yelp_business_file = '../../../resources/dataset/LasVegas_Restaurants_Rimsha_Preprocessed.csv'
+    dataset_file = '../../../resources/dataset/final_lasvegas_dataset.csv'
     yelp_output_file = '../../../resources/dataset/scraped_data.csv'
-    df = pd.read_csv(yelp_business_file)
+    df = pd.read_csv(dataset_file)
+    # df = df[100:200]
     result = traverse_business_data(df)
     # yelp_df = create_yelp_data_frame_new_format(result)
     yelp_df = pd.DataFrame.from_dict(result, orient='columns')
-    yelp_df.to_csv(yelp_output_file, encoding='utf-8', index=False, mode='w', header=True)
+    yelp_df.to_csv(yelp_output_file, encoding='utf-8', index=False, mode='a', header=True)
     print("finished...")
+    program_end_time = round(time.time() * 1000)
+    print("\nProgram run time:", (program_end_time - program_start_time)/1000, "s")
 
--- src/data_collection/yelp/yelp_scraper.py
+++ src/data_collection/yelp/yelp_scraper.py
@@ -35,6 +35,47 @@ def scrap_business_info(info, result_data):
                 result_data[key] = value
 
 
+def fill_missing_values_As_Null(result_data):
+    if YELP_COl_TAKES_RESERVATIONS not in result_data: result_data[YELP_COl_TAKES_RESERVATIONS] = "Null"
+    if YELP_COl_DELIVERY not in result_data: result_data[YELP_COl_DELIVERY] = "Null"
+    if YELP_COl_TAKE_OUT not in result_data: result_data[YELP_COl_TAKE_OUT] = "Null"
+    if YELP_COl_ACCEPTS_CREDIT_CARD not in result_data: result_data[YELP_COl_ACCEPTS_CREDIT_CARD] = "Null"
+    if YELP_COl_ACCEPTS_APPLE_PAY not in result_data: result_data[YELP_COl_ACCEPTS_APPLE_PAY] = "Null"
+    if YELP_COl_GOOD_FOR not in result_data: result_data[YELP_COl_GOOD_FOR] = "Null"
+    if YELP_COl_PARKING not in result_data: result_data[YELP_COl_PARKING] = "Null"
+    if YELP_COl_BIKE_PARKING not in result_data: result_data[YELP_COl_BIKE_PARKING] = "Null"
+    if YELP_COl_GOOD_FOR_KIDS not in result_data: result_data[YELP_COl_GOOD_FOR_KIDS] = "Null"
+    if YELP_COl_GOOD_FOR_GROUPS not in result_data: result_data[YELP_COl_GOOD_FOR_GROUPS] = "Null"
+    if YELP_COl_ATTIRE not in result_data: result_data[YELP_COl_ATTIRE] = "Null"
+    if YELP_COl_AMBIENCE not in result_data: result_data[YELP_COl_AMBIENCE] = "Null"
+    if YELP_COl_NOISE_LEVEL not in result_data: result_data[YELP_COl_NOISE_LEVEL] = "Null"
+    if YELP_COl_ALCOHOL not in result_data: result_data[YELP_COl_ALCOHOL] = "Null"
+    if YELP_COl_OUTDOOR_SEATING not in result_data: result_data[YELP_COl_OUTDOOR_SEATING] = "Null"
+    if YELP_COl_WIFI not in result_data: result_data[YELP_COl_WIFI] = "Null"
+    if YELP_COl_HAS_TV not in result_data: result_data[YELP_COl_HAS_TV] = "Null"
+    if YELP_COl_CATERS not in result_data: result_data[YELP_COl_CATERS] = "Null"
+    if YELP_COl_GOOD_FOR_WORKING not in result_data: result_data[YELP_COl_GOOD_FOR_WORKING] = "Null"
+    if YELP_COl_GENDER_NEUTRAL_RESTROOMS not in result_data: result_data[YELP_COl_GENDER_NEUTRAL_RESTROOMS] = "Null"
+    if YELP_COl_ACCEPTS_BITCOIN not in result_data: result_data[YELP_COl_ACCEPTS_BITCOIN] = "Null"
+    if YELP_COl_ACCEPTS_GOOGLE_PAY not in result_data: result_data[YELP_COl_ACCEPTS_GOOGLE_PAY] = "Null"
+    if YELP_COl_BEST_NIGHTS not in result_data: result_data[YELP_COl_BEST_NIGHTS] = "Null"
+    if YELP_COl_BY_APPOINTMENT_ONLY not in result_data: result_data[YELP_COl_BY_APPOINTMENT_ONLY] = "Null"
+    if YELP_COL_COAT_CHECK not in result_data: result_data[YELP_COL_COAT_CHECK] = "Null"
+    if YELP_COL_DOGS_ALLOWED not in result_data: result_data[YELP_COL_DOGS_ALLOWED] = "Null"
+    if YELP_COL_DRIVE_THRU not in result_data: result_data[YELP_COL_DRIVE_THRU] = "Null"
+    if YELP_COL_GOOD_FOR_DANCING not in result_data: result_data[YELP_COL_GOOD_FOR_DANCING] = "Null"
+    if YELP_COL_HAPPY_HOUR not in result_data: result_data[YELP_COL_HAPPY_HOUR] = "Null"
+    if YELP_COL_HAS_GLUTEN_FREE_OPTIONS not in result_data: result_data[YELP_COL_HAS_GLUTEN_FREE_OPTIONS] = "Null"
+    if YELP_COL_HAS_POOL_TABLE not in result_data: result_data[YELP_COL_HAS_POOL_TABLE] = "Null"
+    if YELP_COL_LIKED_BY_VEGANS not in result_data: result_data[YELP_COL_LIKED_BY_VEGANS] = "Null"
+    if YELP_COL_LIKED_BY_VEGETARIANS not in result_data: result_data[YELP_COL_LIKED_BY_VEGETARIANS] = "Null"
+    if YELP_COL_OFFERS_MILITARY_DISCOUNT not in result_data: result_data[YELP_COL_OFFERS_MILITARY_DISCOUNT] = "Null"
+    if YELP_COL_SMOKING not in result_data: result_data[YELP_COL_SMOKING] = "Null"
+    if YELP_COL_WAITER_SERVICE not in result_data: result_data[YELP_COL_WAITER_SERVICE] = "Null"
+    if YELP_COL_WHEELCHAIR_ACCESSIBLE not in result_data: result_data[YELP_COL_WHEELCHAIR_ACCESSIBLE] = "Null"
+    return result_data
+
+
 """
 Scrap restaurant data from yelp website based on restaurant url
 """
@@ -45,11 +86,12 @@ def scrap_yelp_restaurant_data(restaurant_url):
 
     result_data = {}
 
-    basic_data = soup.find('script', type='application/ld+json')
-    if basic_data and basic_data.text:
-        scrap_basic_info(basic_data.text, result_data)
-    else:
-        print("Basic data not available for restaurant:", restaurant_url)
+    # basic_data = soup.find('script', type='application/ld+json')
+    # basic_data_new = soup.find("dl", {"class": "short-def-list"})
+    # if basic_data and basic_data.text:
+    #     scrap_basic_info(basic_data.text, result_data)
+    # else:
+    #     print("Basic data not available for restaurant:", restaurant_url)
 
     business_info = soup.find("div", {"class": "short-def-list"})
     if business_info:
@@ -57,7 +99,7 @@ def scrap_yelp_restaurant_data(restaurant_url):
     else:
         print("Business info not available for restaurant:", restaurant_url)
 
-    # fill_missing_values_As_None()
+    fill_missing_values_As_Null(result_data)
 
     json_data = json.dumps(result_data)
     # print(json_data)
