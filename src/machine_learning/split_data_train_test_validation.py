2c43b7620e1c3cc3842677454fff9a5431645252
RAJDEEP KAUR <kaurr@usc.edu>
2018-11-11 20:53:28 -0800

    Write split df to csv file

--- src/machine_learning/split_data_train_test_validation.py
+++ src/machine_learning/split_data_train_test_validation.py
@@ -4,11 +4,18 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 def splitData():
-    dataframe = pd.read_csv("../../dataset/final_lasvegas_dataset.csv")
+    dataframe = pd.read_csv("/Users/apple/Desktop/final_lasvegas_dataset.csv")
     X = dataframe.drop(['current_grade','current_score','inspection_grade','inspection_score'],axis=1)
     y = dataframe[['inspection_grade']]
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=1, shuffle=False)
     X_train, X_validate,y_train, y_validate = train_test_split(X_train,y_train, test_size=0.25, random_state=1, shuffle=False)
+    
+    X_train.to_csv("/Users/apple/Desktop/X_train.csv", encoding='utf-8', index=False)
+    y_train.to_csv("/Users/apple/Desktop/y_train.csv", encoding='utf-8', index=False)
+    X_test.to_csv("/Users/apple/Desktop/X_test.csv", encoding='utf-8', index=False)
+    y_test.to_csv("/Users/apple/Desktop/y_test.csv", encoding='utf-8', index=False)
+    X_validate.to_csv("/Users/apple/Desktop/X_validate.csv", encoding='utf-8', index=False)
+    y_validate.to_csv("/Users/apple/Desktop/y_validate.csv", encoding='utf-8', index=False)
     #print(len(X_test))
     #print (len(y_test))
     #print (len(X_train))
