83cb5167aabed5191d14a9671db73c343aeb384b
Devershi Purohit <devership16@gmail.com>
2018-11-13 11:34:58 -0800

    added adaboost model

--- src/machine_learning/adaBoost_model.py
+++ src/machine_learning/adaBoost_model.py
@@ -1,22 +1,88 @@
 import time
+import pandas as pd
+import numpy as np
 from collections import defaultdict
 from INF553_YelpProject.src.utils.inputOutput_utils import csvReader, csvWriter_from_text
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 from INF553_YelpProject.src.data_schema.feature_names import FeatureNames
 from INF553_YelpProject.src.machine_learning.evaluation_metrics import EvaluationMetric
 
 
+#############################
+########## Ajay's code for getting train and test dataset
+
+def construct_dataset():
+    file = '../../resources/dataset/final_lasvegas_dataset.csv'
+    df = pd.read_csv(file)
+    schema_obj = FeatureNames()
+    # df = df[:100]
+    df = df[[schema_obj.COL_RATING,
+             schema_obj.COL_RESTAURANTS_PRICE_RANGE2,
+             schema_obj.COL_INSPECTION_GRADE]]
+    df[schema_obj.COL_RESTAURANTS_PRICE_RANGE2] = df.apply(categorize_price_as_integer, axis=1)
+    df[schema_obj.COL_INSPECTION_GRADE] = df.apply(categorize_grade_with_integer, axis=1)
+
+    print("Dataset Shape:: ", df.shape)
+    return df
+
+def categorize_grade_with_integer(row):
+    schema_obj = FeatureNames()
+    attribute_name = schema_obj.COL_INSPECTION_GRADE
+
+    modified_score = None
+    if row[attribute_name] == 'A' :
+        modified_score = 0
+    else:
+        modified_score = 1
+    return modified_score
+
+
+def categorize_price_as_integer(row):
+    schema_obj = FeatureNames()
+    attribute_name = schema_obj.COL_RESTAURANTS_PRICE_RANGE2
+    modified_price = None
+
+    if row[attribute_name] == "Null": modified_price = -1
+    else: modified_price = int(row[attribute_name])
+    return modified_price
+
+
+def divide_dataset(df):
+    schema_obj = FeatureNames()
+    X = df[[schema_obj.COL_RATING,
+             schema_obj.COL_RESTAURANTS_PRICE_RANGE2]]
+    Y = df[[schema_obj.COL_INSPECTION_GRADE]]
+
+    # print "Dividing data set into training and test set..."
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=62)
+
+    positive_count = 0
+    negative_count = 0
+    for i in range(len(y_test.values)):
+        if y_test.values[i][0] == 1:
+            positive_count+=1
+        else:
+            negative_count+=1
+
+    print("Test data Shape:: ", X_test.shape, 'with ', \
+        'Positives =', positive_count, ', Negatives =', negative_count)
+
+    return X_train, X_test, y_train, y_test
+
+
 def run_adaBoost_model(path):
-    x_test, x_train, x_validate =  csvReader(path + "X_test.csv"), csvReader(path + "X_train.csv"), csvReader(path + "X_validate.csv")
-    y_test, y_train, y_validate =  csvReader(path + "y_test.csv"), csvReader(path + "y_train.csv"), csvReader(path + "y_validate.csv")
     
+    df = construct_dataset()
+    X_train, X_test, y_train, y_test = divide_dataset(df)
     
     model_real = AdaBoostClassifier( DecisionTreeClassifier(max_depth=2),n_estimators=600,learning_rate=1)
 
     model_discrete = AdaBoostClassifier( DecisionTreeClassifier(max_depth=2), n_estimators=600, learning_rate=1.5, algorithm="SAMME")
     
-    
+    model_real.fit(X_train, np.ravel(y_train))
+    model_discrete.fit(X_train, np.ravel(y_train))
 
 
 if __name__=='__main__':   
