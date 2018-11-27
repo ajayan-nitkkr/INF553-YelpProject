50a144e3698e87ddf3a39454680629bbdee7e20d
rimsha <goomer@usc.edu>
2018-11-26 18:20:07 -0800

    Feature Scaling MinMax in Linear SVC

--- src/machine_learning/linear_SVC.py
+++ src/machine_learning/linear_SVC.py
@@ -1,8 +1,4 @@
 
-# coding: utf-8
-
-# In[1]:
-
 
 import numpy as np
 from sklearn.calibration import CalibratedClassifierCV
@@ -17,7 +13,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.data_schema.feature_names import FeatureNames
 from src.machine_learning.evaluation_metrics import EvaluationMetric
-
+from sklearn import preprocessing
 
 
 def construct_dataset():
@@ -27,54 +23,23 @@ def construct_dataset():
     # df = df[:100]
     df = df[[schema_obj.COL_RATING,
              schema_obj.COL_RESTAURANTS_PRICE_RANGE2,
-             schema_obj.COL_INSPECTION_GRADE]]
+             schema_obj.COL_INSPECTION_GRADE,
+             schema_obj.COL_REVIEW_COUNT,
+             schema_obj.COL_VIOLATIONS,
+             schema_obj.COL_CURRENT_DEMERITS,
+        schema_obj.COL_INSPECTION_DEMERITS]]
     df[schema_obj.COL_RESTAURANTS_PRICE_RANGE2] = df.apply(categorize_price_as_integer, axis=1)
     df[schema_obj.COL_INSPECTION_GRADE] = df.apply(categorize_grade_with_integer, axis=1)
 
     print("Dataset Shape:: ", df.shape)
     return df
 
-
-def divide_dataset(df):
-    schema_obj = FeatureNames()
-    X = df[[schema_obj.COL_RATING,
-             schema_obj.COL_RESTAURANTS_PRICE_RANGE2]]
-    Y = df[[schema_obj.COL_INSPECTION_GRADE]]
-
-    # print "Dividing data set into training and test set..."
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
-
-    positive_count = 0
-    negative_count = 0
-    for i in range(len(y_test.values)):
-        if y_test.values[i][0] == 1:
-            positive_count+=1
-        else:
-            negative_count+=1
-
-    print("Test data Shape:: ", X_test.shape, 'with ', \
-        'Positives =', positive_count, ', Negatives =', negative_count)
-
-    return X_train, X_test, y_train, y_test
-
-
-# def categorize_score_attribute_with_boolean(row):
-#     # attribute_name = 'SCORE'
-#     schema_obj = FeatureNames()
-#     attribute_name = schema_obj.COL_INSPECTION_SCORE
-#
-#     modified_score = None
-#     if row[attribute_name] > 90 and row[attribute_name] <= 100: modified_score = 0
-#     elif row[attribute_name] > 0 and row[attribute_name] <= 90: modified_score = 1
-#     return modified_score
-
-
 def categorize_grade_with_integer(row):
     schema_obj = FeatureNames()
     attribute_name = schema_obj.COL_INSPECTION_GRADE
 
     modified_score = None
-    if row[attribute_name] == 'A' :
+    if row[attribute_name] == 'A':
         modified_score = 0
     else:
         modified_score = 1
@@ -89,6 +54,33 @@ def categorize_price_as_integer(row):
     if row[attribute_name] == "Null": modified_price = -1
     else: modified_price = int(row[attribute_name])
     return modified_price
+
+
+def divide_dataset(df):
+    schema_obj = FeatureNames()
+    X = df[[schema_obj.COL_RATING,
+             schema_obj.COL_RESTAURANTS_PRICE_RANGE2,
+             schema_obj.COL_REVIEW_COUNT,
+             schema_obj.COL_VIOLATIONS,
+             schema_obj.COL_CURRENT_DEMERITS,
+             schema_obj.COL_INSPECTION_DEMERITS]]
+    Y = df[[schema_obj.COL_INSPECTION_GRADE]]
+
+    # print "Dividing data set into training and test set..."
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=62)
+
+    positive_count = 0
+    negative_count = 0
+    for i in range(len(y_test.values)):
+        if y_test.values[i][0] == 1:
+            positive_count+=1
+        else:
+            negative_count+=1
+
+#     print("Test data Shape:: ", X_test.shape, 'with ', \
+#         'Positives =', positive_count, ', Negatives =', negative_count)
+
+    return X_train, X_test, y_train, y_test
 
 
 
@@ -124,12 +116,15 @@ if __name__ == '__main__':
     df = construct_dataset()
     ###########################################
 
+
     ############# DATA SLICING ################
     X_train, X_test, y_train, y_test = divide_dataset(df)
     # print(X_train,X_test,y_train,y_test)
     ###########################################
 
-
+    min_max_scaler = preprocessing.MinMaxScaler((0,1))
+    X_train = min_max_scaler.fit_transform(X_train)
+    X_test = min_max_scaler.transform(X_test)
 
     ################ TRAINING #################
     svm = LinearSVC()
