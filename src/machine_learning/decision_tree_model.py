c9986325191e67acccaf9a3aa73f629aa0834c1a
rimsha <goomer@usc.edu>
2018-12-01 15:43:04 -0800

    feature selection, for DT

--- src/machine_learning/decision_tree_model.py
+++ src/machine_learning/decision_tree_model.py
@@ -6,6 +6,10 @@ from model_save_and_load import *
 from split_data_train_test_validation import *
 from evaluation_metrics import *
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2, f_classif, mutual_info_classif
+from sklearn import preprocessing
+
 
 global dtree
 def read_dataset(filename):
@@ -36,7 +40,46 @@ def get_eval_metrics(Y_test,Y_pred):
     eval_dict=eval_obj.get_evaluation_metrics(Y_test, Y_pred)
     return eval_dict
 
+def do_feature_selection(X,y,kval):
+    data_chi2_scores = SelectKBest(chi2, k=kval).fit(X, y)
+    selected_feature_indices=data_chi2_scores.get_support(indices=True)
+    chi2_dataset = SelectKBest(mutual_info_classif, k=kval).fit_transform(X, y)
+    return chi2_dataset
+
+
 X_train,X_val,X_test,Y_train,Y_val,Y_test=get_training_test_set("../../resources/dataset/final_lasvegas_dataset.csv")
 dtree = train_DecisionTree(X_train,Y_train)
 Y_pred= predict_health_score(X_test)
 eval_dict=get_eval_metrics(Y_test,Y_pred)
+
+
+"""
+########### CONSTRUCT DATA SET ############
+    df = pd.read_csv('../../resources/dataset/final_lasvegas_dataset_v4.csv')
+    X = df.drop(['inspection_grade'], axis=1)
+    y = df[['inspection_grade']]
+    y.replace('A', 0, inplace=True)
+    y.replace('B', 1, inplace=True)
+    y.replace('C', 1, inplace=True)
+    y.replace('D', 1, inplace=True)
+    y.replace('E', 1, inplace=True)
+    min_max_scaler = preprocessing.MinMaxScaler((0, 1))
+    X = min_max_scaler.fit_transform(X)
+
+    op=open('../../resources/Results/mutual_info_decisionTrees.txt','w')
+    for k in range(1,X.shape[1]+1):
+        datasetX = do_feature_selection(X, y, k)
+
+
+        ############# DATA SLICING ################
+
+        X_train, X_test, y_train, y_test = train_test_split(datasetX, y, test_size=0.2, random_state=1, shuffle=True)
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1, shuffle=True)
+
+        #X_train, X_val, X_test, y_train, y_val, y_test = splitData(filename='../../resources/dataset/final_lasvegas_dataset.csv')
+
+        ###########################################
+
+
+
+"""
