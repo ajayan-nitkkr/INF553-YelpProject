1b1d73936821ce8e232774ffb79975474a7edac6
RAJDEEP KAUR <kaurr@usc.edu>
2018-12-01 14:03:06 -0800

    Remove unnecessary code

--- src/machine_learning/split_data_train_test_validation.py
+++ src/machine_learning/split_data_train_test_validation.py
@@ -1,6 +1,5 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import numpy as np
 
 def splitData(filename):
     dataframe = pd.read_csv(filename)
@@ -14,13 +13,7 @@ def splitData(filename):
 
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=1, shuffle=True)
     X_train, X_val,y_train, y_val = train_test_split(X_train,y_train, test_size=0.25, random_state=1, shuffle=True)
-    
-    #X_train.to_csv("/Users/apple/Desktop/X_train.csv", encoding='utf-8', index=False)
-    #y_train.to_csv("/Users/apple/Desktop/y_train.csv", encoding='utf-8', index=False)
-    #X_test.to_csv("/Users/apple/Desktop/X_test.csv", encoding='utf-8', index=False)
-    #y_test.to_csv("/Users/apple/Desktop/y_test.csv", encoding='utf-8', index=False)
-    #X_validate.to_csv("/Users/apple/Desktop/X_validate.csv", encoding='utf-8', index=False)
-    #y_validate.to_csv("/Users/apple/Desktop/y_validate.csv", encoding='utf-8', index=False)
+
     return X_train,X_val,X_test,y_train,y_val,y_test
 
 
